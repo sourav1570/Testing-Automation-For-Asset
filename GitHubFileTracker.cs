bdad33c435d53c525da44182cedb24bb7396d3f1
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-05-26 15:43:29 +0530

    Updating files to version 2.8

--- GitHubFileTracker.cs
+++ GitHubFileTracker.cs
@@ -1,10 +1,35 @@
+using System;
 using System.Collections.Generic;
+using UnityEditor;
 
+//public static class GitHubFileTracker
+//{
+//    public static HashSet<string> autoDetectedFiles = new HashSet<string>();
+//    public static HashSet<string> deletedFiles = new HashSet<string>();
+
+//    // 👇 Add this line
+//    public static HashSet<string> manuallyRemovedFiles = new HashSet<string>();
+//}
+
+[InitializeOnLoad]
 public static class GitHubFileTracker
 {
-    public static HashSet<string> autoDetectedFiles = new HashSet<string>();
-    public static HashSet<string> deletedFiles = new HashSet<string>();
+    public static HashSet<string> alreadyPushedFiles = new();
+    public static HashSet<string> manuallyRemovedFiles = new();
 
-    // 👇 Add this line
-    public static HashSet<string> manuallyRemovedFiles = new HashSet<string>();
+    static GitHubFileTracker()
+    {
+        LoadPushedFiles();
+    }
+
+    public static void SavePushedFiles()
+    {
+        EditorPrefs.SetString("GitHubPushedFiles", string.Join(";", alreadyPushedFiles));
+    }
+
+    public static void LoadPushedFiles()
+    {
+        string saved = EditorPrefs.GetString("GitHubPushedFiles", "");
+        alreadyPushedFiles = new HashSet<string>(saved.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries));
+    }
 }
