c81e3c1290e0a5a997a97e4284a922efae2e7eb8
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-05-26 15:30:26 +0530

    Updating files to version 2.6

--- NewMonoBehaviourScript1.cs
+++ NewMonoBehaviourScript1.cs
@@ -5,7 +5,7 @@ public class NewMonoBehaviourScript1 : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("FOOL");
     }
 
     // Update is called once per frame
